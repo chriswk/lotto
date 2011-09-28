eadcb95d2c63df736e4dd7226515c8002652dade
Christopher Kolstad <christopher.kolstad@gmail.com>
2011-09-28 22:20:16 +0200

    Added project file and code file

---
+++ build.sbt
@@ -0,0 +1,19 @@
+/** Project **/
+name := "lotto"
+
+organization := "com.chriswk"
+
+version := "1.0-SNAPSHOT"
+
+scalaVersion := "2.9.1"
+
+shellPrompt := { state => System.getProperty("user.name") +"> " }
+
+resolvers ++= Seq("snapshots-repo" at "http://scala-tools.org/repo-snapshots",
+				  "snapshots releases" at "http://scala-tools.org/repo-releases",
+				 "sbt-idea-repo" at "http://mpeltonen.github.com/maven/")
+				
+libraryDependencies ++= Seq("org.specs2" %% "specs2" % "1.6.1" % "test",
+							"org.specs2" %% "specs2-scalaz-core" % "6.0.1" % "test",
+							"junit" % "junit" % "4.8.1")
+				
---
+++ src/main/scala/com/chriswk/lotto/Lotto.scala
@@ -0,0 +1,45 @@
+import scala.collection.SortedSet
+
+object Lotto {
+	def findWinners(correct : SortedSet[Int], extraNumbers : SortedSet[Int], myNumbers : List[SortedSet[Int]]): Unit = {
+		println("Vinnerrekken: [" + correct.mkString(", ") +"]")
+		println("Med tilleggstall [" + extraNumbers.mkString(", ") +"]")
+		myNumbers.foreach((myLine : SortedSet[Int]) => {
+			var correctNumbers : SortedSet[Int] = correct & myLine
+			var extras : SortedSet[Int] = extraNumbers & myLine
+			val intro = "Din rekke: [" + myLine.mkString(", ") +"] ga"
+			val correctN = "deg (" +correctNumbers.size + ") rette [" +correctNumbers.mkString(", ") + "]"
+			val extrasN = "og " + " (" +extras.size +") tilleggstall [" +extras.mkString(", ") +"]"
+			println(intro)
+			println(correctN)
+			println(extrasN)
+		})
+	}
+	
+	def main(args : Array[String]) {
+		var range : Int = args.first.toInt
+		val correctArray = Console.readLine("Tast inn vinnerrekke\n").split(",")
+		var correct:SortedSet[Int] = SortedSet()
+		for(number <- correctArray) {
+			correct += number.toInt
+		}
+		val extrasArray = Console.readLine("Tast inn tilleggs tall\n").split(",")
+		var extras:SortedSet[Int] = SortedSet()
+		for(number <- extrasArray) {
+			extras += number.toInt
+		}
+		
+		var myNumbers : List[SortedSet[Int]] = List(SortedSet())
+		for(i <- 0 until range) {
+			var myArray = Console.readLine("Tast inn din rekke\n").split(",")
+			var my : SortedSet[Int] = SortedSet()
+			for(number <- myArray) {
+				my += number.toInt
+			}
+			myNumbers ::= my
+		}
+		println(correct)
+		println(extras)
+		findWinners(correct, extras, myNumbers)
+	}
+}
